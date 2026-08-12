a69fc3257410570dd739190ff83e22b5cd0fba73
Nisarg3100 <146912789+Nisarg3100@users.noreply.github.com>
2026-08-12 14:15:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+// Access environment variables
+console.log('API Key:', process.env.API_KEY);
+console.log('Database Host:', process.env.DB_HOST);
+console.log('Database Port:', process.env.DB_PORT);
